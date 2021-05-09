88048746e5b30d6c9f0988db05adea2df2c74fef
Qobi <qobitek@gmail.com>
2021-05-09 19:50:07 +0100

    .

---
+++ src/components/view/landingpage.tsx
@@ -0,0 +1,162 @@
+import React,{FC, useRef} from 'react'
+import Header from '../layout/header.js'
+import Footer from '../layout/footer.js'
+import * as font from '../utils/font/font'
+import Result from './resultpage'
+import Nomination from './nominationpage'
+import csshome from '../assets/css/landingpage.module.css'
+import Notification from '../notification'
+import Share from '../share'
+import * as nominationprops from './notificationprops'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { getNominations, useData } from '../utils'
+import Cookies from 'universal-cookie'
+
+const cookies = new Cookies();
+
+Aos.init();
+
+const LandingPage:FC = () => {
+
+    const formBtn = useRef<HTMLButtonElement>(null);
+
+    const { handleOnChange, handleSubmit, data, loading, error, numberofPages, first, last, handlePage, title,
+            handleFocus, handleShare, handleClick, isFocus, share, click, message, tip, setMessage, setClick, setTip,
+            setMovieNominations, nominations, isMobile
+        } = useData({formBtn}) 
+
+    const addNomination = ( index : number ) => {
+        const {max, notification} = nominationprops;
+        if( getNominations().length !== max ){
+            setMessage('');setClick(false);
+            var nominations = [];
+            if( Array.isArray(getNominations())){          
+                nominations = getNominations(); 
+                nominations.push( data[ index ] );
+                cookies.set( 'nomination', JSON.stringify( nominations ) );
+                setMovieNominations();
+                setTimeout( ( ) => {
+                    if( nominations.length === max ){
+                        setMessage(notification.maxNomination.msg);
+                        setTip(notification.maxNomination.tip);
+                        setClick(true);
+                    }
+                },200 )
+            }else{
+                nominations.push( data[ index ] );
+                cookies.set( 'nomination', JSON.stringify( nominations ) );
+                setMovieNominations();
+            }
+        }else{
+            setMessage(notification.exceedNomination.msg);
+            setTip(notification.exceedNomination.tip);
+            setClick(true);
+        }
+    }
+
+    const removeNomination = ( index : number ) => {
+        if( getNominations().length !== 0 ){
+            var nominations  = getNominations();
+            const arrayindex = nominations.indexOf( nominations[ index ] );
+            if( arrayindex > -1 ) nominations.splice( index, 1 );
+            cookies.set( 'nomination', JSON.stringify( nominations ) );
+            setMovieNominations();
+        }
+    }
+
+    const { One, Four, Para, Alpha } = font
+        
+    const searchWord = title;
+        
+    const resultProps = { data, loading, error, searchWord, addNomination, isMobile, handlePage, numberofPages, first, last } 
+
+    const nominationProps = { nominations, isMobile, removeNomination }
+
+    return(
+        <>
+        <Header/>
+            <Notification  click={ click } message={ message } tip={ tip } handleClick={ handleClick } />
+            <Share share={ share } />
+            <div className={` container my-0 my-lg-5 p-0`}>
+                <div className={`w-100 pt-2 pb-5 px-3 px-lg-5 ${csshome.bg}`} >
+                    <div className="pt-3 d-flex w-100 mt-4 text-left text-md-center justify-content-start justify-content-md-center"
+                        data-aos="fade-right" data-aos-duration="2000" data-aos-once="true" >
+                        <One fontStyle={{color:"var(--main-color7)", overflowWrap: "break-word"}} fontclass={csshome.header}>
+                            The Shoppies
+                        </One>
+                    </div>
+                    <div className="pt-4 pb-5 d-flex w-100 text-left text-md-center justify-content-start justify-content-md-center"
+                        data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
+                        <Four fontStyle={{fontFamily : "encode_sans_medium", lineHeight:"25px" }} fontclass={csshome.description}>
+                            Search & Nominate up to 
+                            <div style={{fontSize:"17px", color : "var(--main-color7)"}}>
+                                &nbsp;FIVE (5)&nbsp;
+                            </div> of your favorite movies in the Shopify Movie Awards
+                        </Four>
+                    </div>
+                    <div className="d-flex w-100 text-center justify-content-center" data-aos="fade-in" 
+                        data-aos-duration="2800" data-aos-once="true" >
+                        <Alpha click={()=>handleShare(true)}>
+                            <Para fontStyle={{ cursor:"pointer", color:"#ffc107", fontSize:"1.2em" }}>
+                                Share&nbsp;&nbsp;  
+                                <span><i className="fas fa-share-alt text-warning" /></span>
+                            </Para>
+                        </Alpha>
+                    </div>
+                    <form className="w-100 bg-white rounded mt-5" onSubmit={(e)=>handleSubmit(e)} 
+                        style={{boxShadow:"var(--box-shadow)"}}>
+                        <div className="w-100 d-flex flex-column justify-content-start rounded py-4 px-4">
+                            <label htmlFor="title" className="mb-2">Movie Title</label>
+                            <div className="w-100 d-flex flex-column position-relative justify-content-start">
+                                <input type="text" name="title" id="title" value={ title } onChange={(e)=>handleOnChange(e)} 
+                                    className="rounded pl-3" style={{border:`1px solid ${title.length > 0 ? "var(--main-color7)" : "#e6e6e6"}`, height:"50px", outline:0}} 
+                                    onFocus={ () => handleFocus( true ) } onBlur={()=> handleFocus( title.length > 0 )} />
+                                <div className={`position-absolute h-100 ${!isFocus ? "d-flex" : "d-none"} align-items-center pl-3`} style={{width:"max-content",top:0,left:0}}>
+                                    <i className="fas fa-search" style={{color:"var(--main-color7)"}}/>
+                                </div>
+                                <div className={`position-absolute h-100 ${ title.length > 0 ? "d-flex" : "d-none"} align-items-center pl-3`} style={{width:"max-content",top:0,right:0}}>
+                                    <button className="px-3 bg-transparent" ref={ formBtn }
+                                        style={{border:0, borderLeft:"1px solid var(--main-color7)", color:"var(--main-color7)", height:"50px", outline:0}} >
+                                        Search
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    {isMobile ?
+                    <>
+                    <div className="d-block mt-5">
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item">
+                            <a className={`nav-link active bg-transparent ${csshome.tab}`} data-toggle="tab" href="#resultid">
+                                Result
+                            </a>
+                            </li>
+                            <li className="nav-item">
+                            <a className={`nav-link bg-transparent ${csshome.tab}`} data-toggle="tab" href="#nominationid">
+                                Nominations ( {  Array.isArray(nominations) ? nominations.length : "0" } ) 
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="d-block tab-content w-100 m-0 p-0">
+                        <Result resultProps={ resultProps } />
+                        <Nomination nominationProps={ nominationProps } />
+                    </div>
+                    </>
+                    :
+                    <div className={`d-flex flex-wrap align-items-start justify-content-between`}>
+                        <Result resultProps={ resultProps } />
+                        <Nomination nominationProps={ nominationProps } />
+                    </div>
+                    }
+                </div>
+            </div>
+
+        <Footer/>
+        </>
+    )
+}
+
+export default LandingPage;
---
+++ src/components/view/nominationpage.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react'
+import * as font from '../utils/font/font'
+import ResultItem from './result'
+import csshome from '../assets/css/landingpage.module.css'
+import css from '../assets/css/props.module.css'
+import { IMovies } from '../interface'
+
+interface IProps {
+    nominationProps : { 
+        nominations : IMovies; 
+        isMobile : boolean; 
+        removeNomination : (index:number)=>void 
+    };
+}
+
+const Nomination : FC<IProps> = (props) => {
+
+    const { Three, Five } = font
+
+    const { nominationProps } = props
+
+    const { isMobile, nominations, removeNomination } = nominationProps || {}
+
+    const btnTxt = "Remove"
+
+    const tapprops = isMobile ? "tab-pane fade" : ""
+
+    return(
+        <div className={`${csshome.sticky} col-12 col-lg-6 pr-0 pl-0 pl-lg-3 ${ tapprops }`} >      
+            <div className="d-flex flex-column bg-white rounded my-5 py-4 px-4" style={{boxShadow:"var(--box-shadow)"}}>
+                <div className="d-flex w-100 align-items-center" style={{overflow:"hidden", height:"45px"}}>
+                    <Three fontStyle={{fontFamily:"encode_sans_semi_bold"}}>Nominations</Three>
+                </div>
+                <div className="d-flex w-100 pl-0 mt-4" >
+                    <ul className="d-flex flex-column w-100 m-0 p-0"  >
+                    {Array.isArray( nominations ) && nominations[0] ? nominations.map( ( item, index ) => {
+                        return(
+                            <ResultItem  data={ nominations } item={ item } index={ index } handleNomination={ removeNomination } key={ index }
+                                btnTxt={ btnTxt } variant="nomination" />
+                        )
+                    } )
+                    :
+                    <Five fontclass="mt-2" fontStyle={{opacity:".7"}}>No Nominations</Five>
+                    }
+                    </ul>
+                </div>
+                {nominations.length === 5 &&
+                <div className="d-block w-100 border-top p-0 py-3 text-center">                 
+                    <button className={`px-3 ml-0 ml-md-3 rounded border-0 ${css.button}`} >
+                                Submit Nominations
+                    </button>                        
+                </div>}
+            </div>
+        </div>
+    )
+}
+
+export default Nomination
